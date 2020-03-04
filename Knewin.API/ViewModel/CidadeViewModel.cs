2ca67a2745b4662ab5f7c31a5f2fedb90821260f
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-03-03 22:03:00 -0300

    fronteiras

--- Knewin.API/ViewModel/CidadeViewModel.cs
+++ Knewin.API/ViewModel/CidadeViewModel.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace Knewin.API.ViewModels.CidadeViewModel
 {
     public class CidadeViewModel
     {
         public string Nome { get; set; }
         public double Habitantes { get; set; }
+        public IEnumerable<CidadeViewModel> Fronteiras { get; set; }
     }
 }
